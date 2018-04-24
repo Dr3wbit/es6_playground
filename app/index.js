57d7bc9d5b2c5b06d9c97fc794542d25357ca51b
Dr3wbit <eightbitandrew@gmail.com>
2018-04-24 15:13:59 -0700

    Completed import export demo

--- app/index.js
+++ app/index.js
@@ -1,1 +1,18 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import {addToDom} from './helpers';
+import add from './add';
+import SuperPerson from './person'
+
+addToDom('<h1>LOLOLOLOLOLOLOLMAOBBQROFLCOPTERWTFBB</h1>');
+
+// addToDom(`18+24= ${add(18,24)}`);
+addToDom('<hr>');
+
+const Bob = new SuperPerson('Bob', 'GayBoi');
+const Brett = new SuperPerson('Brett', 'Doink Dude');
+addToDom(Bob.speak('Hello! I\'m better than u'));
+
+addToDom(Brett.speak('Hello! I Suck!'));
+
+addToDom(Brett.xRay());
+
+addToDom(Bob.sayHeroName());
